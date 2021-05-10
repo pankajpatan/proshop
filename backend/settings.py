07e679ee2c12423911e654a14747981933bcc62e
pankajpatan <pankaj.patan3@gmail.com>
2021-05-10 15:17:37 +0530

    changed

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -201,5 +201,3 @@ AWS_STORAGE_BUCKET_NAME  = 'proshopbucket'
 if os.getcwd()=='/app' :
     DEBUG =False
 
-
-django_heroku.settings(locals())
